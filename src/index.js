6d32b3ee06d8a42149979a711ef2029c08105cbc
Shannon Ding <shannon_ding@yahoo.com>
2020-06-11 12:45:51 -0400

    Thursday morning morning

--- src/index.js
+++ src/index.js
@@ -4,34 +4,27 @@ import './index.css';
 
 
 function Panel(props) {
-  const r = props.rgb.r;
-  const g = props.rgb.g;
-  const b = props.rgb.b;
-  const rgba = "rgba(" + r + "," + g + "," + b + ", 0.5)";
-
   return (
-      <div className="panel" style={{backgroundColor: rgba}}>
+      <div className="panel" style={{backgroundColor: props.color}}>
         <img src={"./panel-design.jpg"} alt="panel" />
       </div>
     );
 }
 
 function Panels(props) {
-  const rgbPanels = props.panels.map((v, i) => {
-      return <Panel rgb={v} key={i} />
+  const panels = Object.values(props.panels);
+  const colorPanels = panels.map((v, i) => {
+      return <Panel color={v} key={i} />
     }
     );
-  return (<div key={props.id}>{rgbPanels}</div>);
+  return (<div className="panels">{colorPanels}</div>);
 }
 
 class PanelViewer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: this.props.cueStack.name,
-      cueStack: this.props.cueStack.panels,
-      current: 0,
-      stackLength: this.props.cueStack.panels.length
+      current: 0
     }
     this.handleNextCue = this.handleNextCue.bind(this);
     this.handlePrevCue = this.handlePrevCue.bind(this);
@@ -39,75 +32,80 @@ class PanelViewer extends Component {
 
 
   handleNextCue() {
-    const c = (this.state.current + 1) % this.state.stackLength;
+    var len = Object.keys(this.props.entry['design']).length;
+    const c = (this.state.current + 1) % len;
     this.setState({
-      current: c,
+      current: c
     });
   }
   handlePrevCue() {
-    const c = (this.state.current + this.state.stackLength - 1) % this.state.stackLength;
+    var len = Object.keys(this.props.entry['design']).length;
+    const c = (this.state.current + len - 1) % len;
     this.setState({
-      current: c,
+      current: c
     });
   }
 
   render() {
+    const cueStack = this.props.entry['design'];
     const c = this.state.current;
-    const currentCue = this.state.cueStack[c];
+    const currentCue = cueStack[c + ""];
+    console.log(currentCue);
     return (
     <div className="viewer">
-      <p>Design by {this.state.name}</p>
-      <Panels panels={currentCue.panel} id={this.state.name + "-" + c} />
-      <p>Cue {c} of {this.state.stackLength}</p>
-      <p>Duration: {currentCue.duration}s</p>
+      <p className="designBy">Design by {this.props.entry['user_id']}</p>
+      <Panels panels={currentCue.colors} />
+      <div className="viewer-controls">
+      <p>Cue {c} of {Object.keys(cueStack).length}</p>
+      <p>Duration: {currentCue.duration['$numberInt']}s</p>
       <button onClick={() => this.handlePrevCue()}>prev</button>
       <button onClick={() => this.handleNextCue()}>next</button>
+      </div>
       </div>
     )
   }
 }
 
-class Queue extends Component {
-  renderEntry(value, index, acc, rej, view) {
-    let classes = "undecided";
-    if (value.accepted === null) {
-      if (index === this.props.view) {
-        classes = "viewing";
-      }
+function QueueEntry(props) {
+  let classes = "undecided";
+  const did = props.did;
+    if (did === props.currentView) {
+      classes = "viewing";
     }
-    else if (value.accepted) {
-      classes = " accepted";
-      return;
-    }
-    else {
-      classes = "rejected";
-      return;
-    }
-
     return (
-      <div className="entry" key={index}>
-        <p className={classes}>{value.name}</p>
+      <div className="entry">
+        <p className={classes}>{props.uid}</p>
         <ul className="options">
-          <li className="view" onClick={() => view(index)}>view</li>
-          <li className="accept" onClick={() => acc(index)}>accept</li>
-          <li className="reject" onClick={() => rej(index)}>reject</li>
+          <li className="view" onClick={() => props.view(did)}>view</li>
+          <li className="accept" onClick={() => props.acc(did)}>accept</li>
+          <li className="reject" onClick={() => props.rej(did)}>reject</li>
         </ul>
       </div>
     );
-  }
+}
+
+class Queue extends Component {
   render() {
-    const queue = this.props.queue.map(
-      (value, index) => {
-        return this.renderEntry(value, index, this.props.onAccept, this.props.onReject, this.props.onView);
+    const queue = Object.values(this.props.queue);
+    const entries = queue.map(
+      (entry) => {
+        return <QueueEntry did={entry['_id']['$oid']} 
+                          uid={entry['user_id']}
+                          acc={this.props.onAccept}
+                          rej={this.props.onReject}
+                          view={this.props.onView} 
+                          currentView={this.props.view}
+                          key={entry['_id']['$oid']} />
       }
     );
-    console.log(queue.length);
     if (queue.length === 0) {
-      console.log("wack yo");
+      return (<div className="panel-queue" >
+      <p>No entries</p>
+      </div>)
     }
     return (
-      <div className="panel-queue">
-      {queue}
+      <div className="panel-queue" >
+      {entries}
       </div>
     );
 
@@ -115,66 +113,48 @@ class Queue extends Component {
 }
 
 let QUEUE = [
-  {
-    "name": "Jerry",
-    "panels": [
-    { "duration": 5, "panel": [{"r": 255, "g": 0, "b": 0}, {"r": 0, "g": 255, "b": 0}, {"r": 0, "g": 0, "b": 255}] },
-    { "duration": 5, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 5, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 0
-  },
-  {
-    "name": "Kerry",
-    "panels": [
-    { "duration": 3, "panel": [{"r": 100, "g": 0, "b": 0}] },
-    { "duration": 3, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 3, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 1
-  },
-  {
-    "name": "Lerry",
-    "panels": [
-    { "duration": 1, "panel": [{"r": 100, "g": 0, "b": 0}] },
-    { "duration": 1, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 1, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 2
-  },
-  {
-    "name": "Merry",
-    "panels": [
-    { "duration": 5, "panel": [{"r": 100, "g": 0, "b": 0}] },
-    { "duration": 5, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 5, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 3
-  },
-  {
-    "name": "Nerry the Platypus",
-    "panels": [
-    { "duration": 3, "panel": [{"r": 100, "g": 0, "b": 0}] },
-    { "duration": 3, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 3, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 4
-  },
-  {
-    "name": "Oerry",
-    "panels": [
-    { "duration": 1, "panel": [{"r": 100, "g": 0, "b": 0}] },
-    { "duration": 1, "panel": [{"r": 0, "g": 100, "b": 0}] },
-    { "duration": 1, "panel": [{"r": 0, "g": 0, "b": 100}] },
-    ],
-    "accepted": null,
-    "designID": 5
-  }
+   {
+        "_id":  {"$oid":"5edc39064754c6a2c030e10f"}, // unique for each request
+        "user_id":  "Srinu Lade 1",
+        "design":   {
+            "0":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            },
+            "1":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            }
+        }
+    },
+   {
+        "_id":  {"$oid":"5edc39064754c6a2c030e10g"}, // unique for each request
+        "user_id":  "Srinu Lade 2",
+        "design":   {
+            "0":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            },
+            "1":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            }
+        }
+    },
+    {
+        "_id":  {"$oid":"5edc39064754c6a2c030e10h"}, // unique for each request
+        "user_id":  "Srinu Lade 3",
+        "design":   {
+            "0":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            },
+            "1":{
+                "colors":{"0":"#808080","1":"#808080","2":"#808080","3":"#808080","4":"#808080","5":"#808080","6":"#808080","7":"#808080","8":"#808080","9":"#808080","10":"#808080","11":"#808080","12":"#808080","13":"#808080","14":"#808080","15":"#808080","16":"#808080","17":"#808080","18":"#808080","19":"#808080","20":"#808080","21":"#808080","22":"#808080","23":"#808080","24":"#808080","25":"#808080","26":"#808080","27":"#808080","28":"#808080","29":"#808080","30":"#808080","31":"#808080","32":"#808080","33":"#808080","34":"#808080","35":"#808080","36":"#808080","37":"#808080","38":"#808080","39":"#808080","40":"#808080","41":"#808080","42":"#808080","43":"#808080","44":"#808080","45":"#808080","46":"#808080","47":"#808080","48":"#808080","49":"#808080","50":"#808080","51":"#808080"},
+                "duration":{"$numberInt":"1"}
+            }
+        }
+    }
 ]
 
 class QueueView extends Component {
@@ -183,68 +163,88 @@ class QueueView extends Component {
 
     this.state = {
       queue: QUEUE,
-      accepted: [],
-      rejected: [],
-      currentView: null
+      currentViewID: null
     }
+
+    this.removeDesignID = this.removeDesignID.bind(this);
+    this.removeFromQueue = this.removeFromQueue.bind(this);
+    this.getEntryFromID = this.getEntryFromID.bind(this);
+    this.sendTheme = this.sendTheme.bind(this);
+    this.handleAccept = this.handleAccept.bind(this);
+    this.handleReject = this.handleReject.bind(this);
+    this.handleView = this.handleView.bind(this);
+
   }
 
-  handleAccept(i) {
-    let queue = this.state.queue.slice();
-    queue[i].accepted = true;
-    this.state.accepted.push(i);
-    let c = this.state.currentView
-    if (i === c) {
+  removeDesignID(id) {
+    // go to database and remove with designID
+
+  }
+
+  removeFromQueue(id, queue) {
+    return queue.filter(obj => (obj['_id']['$oid'] !== id));
+  }
+
+  getEntryFromID(id, queue) {
+    for (var i = 0; i < queue.length; i++) {
+      if (queue[i]['_id']['$oid'] === id) {
+        return queue[i];
+      }
+    }
+    throw "Invalid design ID";
+  }
+
+  async sendTheme(id) {
+    const entry = this.getEntryFromID(id, this.state.queue);
+      // code copied from previous sendTheme button in front end
+    const res = await fetch("http://pbridge.adm.cs.cmu.edu:5000/theme", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: entry['user_id'], panels: entry['design'] }), // this should be written however necessary to match backend code
+    });
+
+    const ans = await res.json();
+    console.log(ans);
+  }
+
+  handleAccept(id) {
+    let c = this.state.currentViewID;
+    if (id === c) {
       c = null;
     }
     this.setState({
-      queue: queue,
-      currentView: c
+      queue: this.removeFromQueue(id, this.state.queue),
+      currentViewID: c
     });
 
-    const sendTheme = async () => {
-      const response = await fetch("http://pbridge.adm.cs.cmu.edu:5000/theme",
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: this.state.queue[i].name,
-          panels: this.state.queue[i].panels
-        })
-      });
-      const res = await response.json();
-      console.log(res);
-      return res;
-    }
-
-    sendTheme();
+    this.sendTheme(id);
+    this.removeDesignID(id);
   }
 
-  handleReject(i) {
-    let queue = this.state.queue.slice();
-    queue[i].accepted = false;
-    this.state.rejected.push(i);
-    let c = this.state.currentView
-    if (i === c) {
+  handleReject(id) {
+    let c = this.state.currentViewID;
+    if (id === c) {
       c = null;
     }
     this.setState({
-      queue: queue,
-      currentView: c
+      queue: this.removeFromQueue(id, this.state.queue),
+      currentViewID: c
     });
+
+    this.removeDesignID(id);
   }
 
-  handleView(i) {
-    if (i === this.state.currentView) {
+  handleView(id) {
+    if (id === this.state.currentViewID) {
       this.setState({
-        currentView: null,
+        currentViewID: null,
       });
     }
     else {
       this.setState({
-        currentView: i,
+        currentViewID: id,
       });
     }
     
@@ -254,12 +254,13 @@ class QueueView extends Component {
 
   render() {
     let currentView;
-    if (this.state.currentView === null) {
+    console.log(this.state.currentViewID);
+    if (this.state.currentViewID === null) {
       currentView = <p>No current view</p>;
     }
     else {
-      let cueStack = this.state.queue[this.state.currentView];
-      currentView = <PanelViewer cueStack={cueStack} key={this.state.currentView} />
+      const entry = this.getEntryFromID(this.state.currentViewID, this.state.queue);
+      currentView = <PanelViewer entry={entry} key={entry['_id']['$oid']} />
     }
 
 
@@ -271,7 +272,7 @@ class QueueView extends Component {
                 onAccept={(i) => this.handleAccept(i)}
                 onReject={(i) => this.handleReject(i)} 
                 onView={(i) => this.handleView(i)} 
-                view={this.state.currentView} />
+                view={this.state.currentViewID} />
         </div>
         {currentView}
       </div>
